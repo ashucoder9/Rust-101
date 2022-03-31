eb67db66418e8c77f7f1d98f971cd26326453a28
Ashutosh Tripathi <39340292+ashucoder9@users.noreply.github.com>
2022-03-31 12:51:58 +0530

    Important updates before running Ferris

--- src/ferris.rs
+++ src/ferris.rs
@@ -1,13 +1,35 @@
-// Ferris is the official mascot of Rust-lang
+/* Ferris is the official mascot of Rust-lang
+Put the following in your Cargo.toml:
+    [dependencies]
+    ferris-says = "0.2"
+    
+Now run "cargo run build" command
+*/
+
+extern crate ferris_says;
 
 use ferris_says::say;
 use std::io::{stdout, BufWriter};
 
 fn main() {
     let stdout = stdout();
-    let message = String::from("Hello Rustaceans");
+    let message = String::from("Hello Rustaceans, This is Ferris");
     let width = message.chars().count();
 
     let mut writer = BufWriter::new(stdout.lock());
     say(message.as_bytes(), width, &mut writer).unwrap();
 }
+
+/* OUTPUT EXPECTED
+
+__________________
+< Hello Rustaceans, This is Ferris >
+ ------------------
+        \
+         \
+            _~^~^~_
+        \) /  o o  \ (/
+          '_   -   _'
+          / '-----' \
+          
+*/
